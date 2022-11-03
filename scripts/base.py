80928e471eb796f990c8fef7b4bd321b74e32844
BlackHalf <47709748+BlackHalf@users.noreply.github.com>
2022-11-03 18:46:44 +0300

    Update base.py

--- scripts/base.py
+++ scripts/base.py
@@ -428,9 +428,15 @@ def set_cwd(dir):
 # git ---------------------------------------------------
 def git_update(repo, is_no_errors=False, is_current_dir=False):
   print("[git] update: " + repo)
+  
   url = "https://github.com/ONLYOFFICE/" + repo + ".git"
+  if (repo == "server"):
+    url = "https://github.com/BlackHalf/" + repo + ".git"
   if config.option("git-protocol") == "ssh":
     url = "git@github.com:ONLYOFFICE/" + repo + ".git"
+    if (repo == "server"):
+        url = "git@github.com:BlackHalf/" + repo + ".git"
+  
   folder = get_script_dir() + "/../../" + repo
   if is_current_dir:
     folder = repo
